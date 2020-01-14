b54e5eae85c4d04ff146740863c5c376558c9698
George Davila <georgedavila@Georges-MacBook-Pro.local>
2020-01-13 20:17:57 -0500

    finished mvp

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,20 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+import React from "react";
+
+const TodoList = props => {
+  let verboseClassName = "item";
+  if (props.item.completed) {
+    verboseClassName = verboseClassName + " completed";
+  }
+
+  const handleClick = () => {
+    props.toggleCompleted(props.item.id);
+  };
+
+  return (
+    <div onClick={handleClick} className={verboseClassName}>
+      <p>{props.item.name}</p>
+    </div>
+  );
+};
+
+export default TodoList; 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import ReactDOM from 'react-dom';
 
-render(<App />, document.getElementById('root'));
+
+const data = [
+    {
+      name: "Sleep",
+      id: 123,
+      completed: false
+    },
+    {
+      name: "Play Video Games",
+      id: 1246,
+      completed: false
+    },
+    {
+      name: "Eat",
+      id: 1237,
+      completed: false
+    },
+ 
+  ];
+export default data;
+
+
+
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
